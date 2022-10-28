92bf246c20dfa4987ca5752bc8466a7668fd0334
Andre Melo <andre.fabian.melo@gmail.com>
2022-10-27 23:48:14 -0300

    feat: update global styles with custom sidebar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,9 +6,29 @@ export default createGlobalStyle`
     padding: 0;
     outline: 0;
     box-sizing: border-box;
-    /* overflow: hidden; */
     font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   }
+
+   /* width */
+::-webkit-scrollbar {
+  width: 8px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #1a1a1a;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #444;
+  border-radius: 50px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 
   body {
     background: #1a1a1a;
